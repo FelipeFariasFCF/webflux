d71be7ec0dc4bf40015113a7d435fef408e9af7c
FelipeFariasFCF <felipe.fariasfcf@outlook.com>
2023-04-16 11:56:06 -0300

    add impl to find all user contract

--- src/main/java/br/com/felipefarias/webflux/repository/UserRepository.java
+++ src/main/java/br/com/felipefarias/webflux/repository/UserRepository.java
@@ -4,6 +4,7 @@ import br.com.felipefarias.webflux.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Repository
@@ -12,11 +13,15 @@ public class UserRepository {
 
     private final ReactiveMongoTemplate mongoTemplate;
 
-    public Mono<User> save(final User user){
+    public Mono<User> save(final User user) {
         return mongoTemplate.save(user);
     }
 
     public Mono<User> findById(String id) {
         return mongoTemplate.findById(id, User.class);
+    }
+
+    public Flux<User> findAll() {
+        return mongoTemplate.findAll(User.class);
     }
 }
