f171a8612ce7668bf1e68af2d4e3f5bff364e520
FelipeFariasFCF <felipe.fariasfcf@outlook.com>
2023-04-16 09:50:38 -0300

    add mapper to user and impl to sabe contract user controller

---
+++ src/main/java/br/com/felipefarias/webflux/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package br.com.felipefarias.webflux.mapper;
+
+import br.com.felipefarias.webflux.entity.User;
+import br.com.felipefarias.webflux.model.request.UserRequest;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.NullValueCheckStrategy;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(
+        componentModel = "spring",
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS
+)
+public interface UserMapper {
+
+    @Mapping(target = "id", ignore = true)
+    User toEntity(final UserRequest request);
+}
---
+++ src/main/java/br/com/felipefarias/webflux/service/UserService.java
@@ -0,0 +1,21 @@
+package br.com.felipefarias.webflux.service;
+
+import br.com.felipefarias.webflux.entity.User;
+import br.com.felipefarias.webflux.mapper.UserMapper;
+import br.com.felipefarias.webflux.model.request.UserRequest;
+import br.com.felipefarias.webflux.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository repository;
+    private final UserMapper mapper;
+
+    public Mono<User> save(final UserRequest request) {
+        return repository.save(mapper.toEntity(request));
+    }
+}
